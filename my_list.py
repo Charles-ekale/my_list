9bcda806547e7a363e27ccad89bd128b0dca8b16
charles ekale <99409935+Charles-ekale@users.noreply.github.com>
2024-03-30 10:40:05 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,19 @@
+my_list =[]
+
+list_2 = [10, 20, 30, 40]
+list_2 [1]=15
+
+my_list.append(list_2)
+
+list_3 = [50, 60, 70]
+
+list_2.extend(list_3)
+
+del list_2[6]
+
+list_2.sort()
+
+print("The sorted list is: ", list_2)
+print("The Index of '30' is: ", list_2.index(30))
+
+ 
